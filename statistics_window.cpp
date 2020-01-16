64f24d33e2dd5f49f7d98f108540d08ede4c4153
Hyunhong Kim <pol4bear@gmail.com>
2020-01-16 15:50:23 +0900

    Update StatisticsWindow

--- statistics_window.cpp
+++ statistics_window.cpp
@@ -1,17 +1,17 @@
 #include "statistics_window.h"
 #include "ui_statistics_window.h"
 
-StatisticsWindow::StatisticsWindow(const PacketStatistics *ps, QWidget *parent) :
+StatisticsWindow::StatisticsWindow(PacketStatistics &ps, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::StatisticsWindow)
 {
-    mac_endpoints = ps->get_mac_endpoints();
-    mac_conversations = ps->get_mac_conversations();
-    ip_endpoints = ps->get_ip_endpoints();
-    ip_conversations = ps->get_ip_conversations();
+    mac_endpoints = ps.get_mac_endpoints();
+    mac_conversations = ps.get_mac_conversations();
+    ip_endpoints = ps.get_ip_endpoints();
+    ip_conversations = ps.get_ip_conversations();
 
     ui->setupUi(this);
-    this->setWindowTitle(QString::fromStdString(ps->get_file_name()));
+    this->setWindowTitle(QString::fromStdString(ps.get_file_name()));
     initialize();
 }
 
@@ -22,22 +22,22 @@ StatisticsWindow::~StatisticsWindow()
 
 void StatisticsWindow::initialize()
 {
-    for (MacEndpoints::const_iterator it = mac_endpoints->begin(); it != mac_endpoints->end(); it++) {
+    for (auto it = mac_endpoints->begin(); it != mac_endpoints->end(); it++) {
         mac_endpoints_model.append(it->first, it->second);
     }
     ui->tv_mac_1->setModel(&mac_endpoints_model);
 
-    for (MacConversations::const_iterator it = mac_conversations->begin(); it != mac_conversations->end(); it++) {
+    for (auto it = mac_conversations->begin(); it != mac_conversations->end(); it++) {
         mac_conversations_model.append(it->first, it->second);
     }
     ui->tv_mac_2->setModel(&mac_conversations_model);
 
-    for (IpEndpoints::const_iterator it = ip_endpoints->begin(); it != ip_endpoints->end(); it++) {
+    for (auto it = ip_endpoints->begin(); it != ip_endpoints->end(); it++) {
         ip_endpoints_model.append(it->first, it->second);
     }
     ui->tv_ip_1->setModel(&ip_endpoints_model);
 
-    for (IpConversations::const_iterator it = ip_conversations->begin(); it != ip_conversations->end(); it++) {
+    for (auto it = ip_conversations->begin(); it != ip_conversations->end(); it++) {
         ip_conversations_model.append(it->first, it->second);
     }
     ui->tv_ip_2->setModel(&ip_conversations_model);
--- statistics_window.h
+++ statistics_window.h
@@ -16,15 +16,15 @@ class StatisticsWindow : public QDialog
     Q_OBJECT
 
 public:
-    explicit StatisticsWindow(const pol4b::PacketStatistics *ps, QWidget *parent = nullptr);
+    explicit StatisticsWindow(pol4b::PacketStatistics &ps, QWidget *parent = nullptr);
     ~StatisticsWindow();
 
 private:
     Ui::StatisticsWindow *ui;
-    const MacEndpoints *mac_endpoints;
-    const MacConversations *mac_conversations;
-    const IpEndpoints *ip_endpoints;
-    const IpConversations *ip_conversations;
+    PacketStatistics::MacEndpoints *mac_endpoints;
+    PacketStatistics::MacConversations *mac_conversations;
+    PacketStatistics::IpEndpoints *ip_endpoints;
+    PacketStatistics::IpConversations *ip_conversations;
     MacEndpointsModel mac_endpoints_model;
     MacConversationsModel mac_conversations_model;
     IpEndpointsModel ip_endpoints_model;
